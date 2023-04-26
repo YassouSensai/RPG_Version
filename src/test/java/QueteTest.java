e07e4d99b5bd5ac6e925f9924bcb7e6be3524eb6
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-26 20:33:30 +0200

    Création des méthodes de test pour les méthodes estQueteFinale() et possedePrecondition

--- src/test/java/QueteTest.java
+++ src/test/java/QueteTest.java
@@ -5,10 +5,24 @@ class QueteTest {
     @org.junit.jupiter.api.Test
     void estQueteFinale() {
         System.out.println("Test de la methode estQuesteFinale");
+
+        Quete [] tabQuetes = {new Quete("1|(4, 3)|()|2|100|explorer pic de Bhanborim"), new Quete("0|(1, 1)|((3,4),)|4|350|vaincre Araignée lunaire")};
+        boolean [] tabReponses = {false, true};
+
+        for (int i=0; i < tabQuetes.length; i++) {
+            assertTrue(tabQuetes[i].estQueteFinale() == tabReponses[i]);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void possedePreconditions() {
         System.out.println("Test de la methode possedePrecondition");
+
+        Quete [] tabQuetes = {new Quete("1|(4, 3)|()|2|100|explorer pic de Bhanborim"), new Quete("0|(1, 1)|((3,4),)|4|350|vaincre Araignée lunaire")};
+        boolean [] tabReponses = {false, true};
+
+        for (int i=0; i < tabQuetes.length; i++) {
+            assertTrue(tabQuetes[i].estQueteFinale() == tabReponses[i]);
+        }
     }
 }
