3fae13e56a406c797a77452524bf6c5f9bef3cbf
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-26 14:52:56 +0200

    Premier essai de lecture d'un scenario dans la classe Client (modification de paramètre dans pom.xml)

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,13 @@
+import java.io.File;
+
 public class Client {
     public static void main (String [] args) {
+        File fichierScenario0 = new File("scenarios"+ File.separator+"scenario_0.txt");
+
+        LectureFichierTexte premiereLecture = new LectureFichierTexte();
+        Scenario scenario0 = premiereLecture.lecture(fichierScenario0);
+
+        System.out.println(scenario0);
 
     }
 }
