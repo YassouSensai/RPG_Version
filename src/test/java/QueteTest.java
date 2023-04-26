6ed74e3fc5dd2b7ec1bb3e4e79c174b037e20c11
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-26 20:26:01 +0200

    Création d'une classe de test pour la classe Quete (+ajout des dépendances dans le fichier pom.xml)

---
+++ src/test/java/QueteTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueteTest {
+
+    @org.junit.jupiter.api.Test
+    void estQueteFinale() {
+        System.out.println("Test de la methode estQuesteFinale");
+    }
+
+    @org.junit.jupiter.api.Test
+    void possedePreconditions() {
+        System.out.println("Test de la methode possedePrecondition");
+    }
+}
