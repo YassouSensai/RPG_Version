fa771414aaa0d93cd1f4bf660a227b60f2ea4754
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-30 19:41:33 +0200

    Début du Niveau 1 - ajout d'une classe NiveauUN

---
+++ src/main/java/NiveauUN.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class NiveauUN {
+    Scenario chScenarioEnCour;
+    Quete queteFinale;
+    ArrayList<Quete> quetesSansPreconditions;
+    ArrayList<Quete> quetesAvecPreconditions;
+    int experienceAccumulee;
+    int experienceNecessaire;
+
+    public NiveauUN() {}
+}
