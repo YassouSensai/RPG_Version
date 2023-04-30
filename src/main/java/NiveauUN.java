32ee5b188d1c32fe382824636bd01955265ce285
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-30 20:03:16 +0200

    Ajout de la méthode toString() de la classe NiveauUn

--- src/main/java/NiveauUN.java
+++ src/main/java/NiveauUN.java
@@ -8,9 +8,21 @@ public class NiveauUN {
     int experienceAccumulee = 0;
     int experienceNecessaireQueteFinale;
 
+    /**
+     * Constructeur de la classe NiveauUN
+     * @param parScenario
+     */
     public NiveauUN(Scenario parScenario) {
         scenarioEnCour = parScenario;
         queteFinale = parScenario.queteFinale();
         experienceNecessaireQueteFinale = parScenario.queteFinale().getChExperience();
+    }
+
+    /**
+     * Methode toString de la classe NiveauUN
+     * @return String
+     */
+    public String toString() {
+        return "Niveau 1 :\nQuete finale : " + queteFinale.toString() + "\nNombre total de quetes : " + scenarioEnCour.chQuetes.size();
     }
 }
