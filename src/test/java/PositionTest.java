e216e8bcf503e05ca154586ebda814c00f662e71
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-04-30 23:27:05 +0200

    mise à jour de la méthode de test pour la méthode deplacement() de la classe Position

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -11,9 +11,11 @@ class PositionTest {
         Position [] tabPositions = {new Position(0,0), new Position(5,5), new Position(new Quete("1|(3, 1)|()|2|50|dialoguer avec Alaric le mage noir"))};
         int [] tabReponses = {0, 10, 4, 10, 0, 6, 4, 6, 0};
 
+        int incrementation = 0;
         for (int i=0; i < tabPositions.length; i++) {
             for (int j=0; j < tabPositions.length; j++) {
-                assertTrue(tabPositions[i].deplacement(tabPositions[j]) == tabReponses[i+j]);
+                assertEquals(tabPositions[i].deplacement(tabPositions[j]), tabReponses[incrementation]);
+                incrementation++;
             }
         }
     }
