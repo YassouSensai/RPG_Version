2ba7dd1c0a3efcda9b5abbaa682aa09f68ffb707
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-05-01 22:58:56 +0200

    mise à jour

--- src/test/java/NiveauUNParametreTest.java
+++ src/test/java/NiveauUNParametreTest.java
@@ -1,15 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NiveauUNParametreTest {
 
     @Test
     void solutionEfficace() {
-
+        System.out.println("Test de la méthode solutionEfficace()");
     }
 
     @Test
     void solutionExhaustive() {
+        System.out.println("Test de la méthode solutionExhaustive()");
     }
 }
