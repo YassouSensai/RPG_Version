eea8f22e94ae713d17b1d7dcfff836a57ec851bb
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-05-03 00:14:07 +0200

    mise a jour de la classe NiveauUNParametre.java

--- src/main/java/NiveauUNParametre.java
+++ src/main/java/NiveauUNParametre.java
@@ -6,7 +6,7 @@ public class NiveauUNParametre {
     Quete queteFinale;
     int experienceAccumulee = 0;
     int experienceNecessaireQueteFinale;
-    int[] quetesRealisee = {1};
+    ArrayList<Integer> quetesRealisee = new ArrayList<>();
     Position positionDepart = new Position(0,0);
     Position positionSuivante;
     int dureeAccumulee = 0;
@@ -19,110 +19,64 @@ public class NiveauUNParametre {
         scenarioEnCour = parScenario;
         queteFinale = parScenario.queteFinale();
         experienceNecessaireQueteFinale = parScenario.queteFinale().getChExperience();
-        quetesRealisee = new int[scenarioEnCour.getChQuetes().size()];
     }
 
     /**
-     * cette methode permer de verifier qu'une quete a bien été réalisée
+     * cette methode permet de verifier qu'une quete a bien été réalisée
      * @param precondition
      * @return boolean
      */
     private boolean estRealisee(int precondition) {
-        return Arrays.asList(quetesRealisee).contains(precondition);
+        return quetesRealisee.contains(precondition);
     }
 
-    /**
-     * cette methode permet d'ajouter une quete à la solution et de réaliser
-     * les manipulations supplémentaires nécessaires
-     * @param indice
-     */
-    private void realisonLaQuete(Quete queteEnCour, int indice, Quete[] solution, ArrayList<Quete> scenarioListe) {
-        solution[indice] = queteEnCour;
-        quetesRealisee[indice] = queteEnCour.getChNumero();
-        scenarioListe.remove(queteEnCour);
-        experienceAccumulee += queteEnCour.chExperience;
-    }
 
     /**
-     * Cette methode permet de faire les étapes nécessaires si la prochaine quete à réaliser possede une seule precondition
-     * @param preconditions
+     * Cette methode permet de verifier que les preconditions d'une quete ont bien été vérifié
      * @param queteEnCour
-     * @param indice
-     * @param solution
-     * @param scenarioListe
+     * @return boolean
      */
-    private void realisationLaQueteSiUnePrecondition(int [][] preconditions, Quete queteEnCour, int indice, Quete[] solution, ArrayList<Quete> scenarioListe) {
-        if (preconditions[0][1] == 0 && estRealisee(preconditions[0][0])) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
-        }
-        if (estRealisee(preconditions[0][1]) || estRealisee(preconditions[0][0])) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
+    private boolean preconditionsValidee(Quete queteEnCour) {
+        int[][] preconditions = queteEnCour.getChPreconditions();
+
+    }
+
+
+    /**
+     * la methode realisonLaQuete() permet de faire les differents ajouts (solution, experience, duree, ...)
+     * @param queteEnCour
+     * @param solution
+     */
+    private void realisonLaQuete(Quete queteEnCour, ArrayList<Quete> solution) {
+        quetesRealisee.add(queteEnCour.getChNumero());
+        solution.add(queteEnCour);
+
+        if (queteEnCour != queteFinale) {
+            experienceAccumulee += queteEnCour.getChExperience();
         }
     }
 
-    /**
-     * Cette methode permet de faire les étapes nécessaires si la prochaine quete à réaliser possede deux preconditions
-     * @param preconditions
-     * @param queteEnCour
-     * @param indice
-     * @param solution
-     * @param scenarioListe
-     */
-    private void realisationLaQueteSiDeuxPreconditions(int [][] preconditions, Quete queteEnCour, int indice, Quete[] solution, ArrayList<Quete> scenarioListe) {
-        if (estRealisee(preconditions[0][0]) && estRealisee(preconditions[1][0])) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
-        }
-        if (estRealisee(preconditions[0][0]) && (estRealisee(preconditions[1][1]) || estRealisee(preconditions[1][0]))) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
-        }
-        if ((estRealisee(preconditions[0][1]) || estRealisee(preconditions[0][0])) && estRealisee(preconditions[1][0])) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
-        }
-        if ((estRealisee(preconditions[0][1]) || estRealisee(preconditions[0][0])) && (estRealisee(preconditions[1][1]) || estRealisee(preconditions[1][0]))) {
-            realisonLaQuete(queteEnCour, indice, solution, scenarioListe);
-        }
-    }
+
+
 
     /**
      * la methode solutionEfficace() renvoie un tableau avec les quetes dans l'ordre pour une solution dite efficace
-     * @return Quete[]
+     * @return ArrayList<Quete>
      */
-    public Quete[] solutionEfficace() {
-        ArrayList<Quete> scenarioListe = scenarioEnCour.getChQuetes();
-        Quete[] solution = new Quete[scenarioListe.size()];
+    public ArrayList<Quete> solutionEfficace() {
+        quetesRealisee = new ArrayList<>();
+        ArrayList<Quete> solution = new ArrayList<>();
 
-        while (experienceAccumulee < experienceNecessaireQueteFinale || !estRealisee(0)) {
-            for (int i=0; i < scenarioListe.size(); i++) {
-                Quete queteEnCour = scenarioListe.get(i);
+        while (!estRealisee(0)) {
+            for (Quete queteEnCour : scenarioEnCour.getChQuetes()) {
 
-                if (!queteEnCour.possedePreconditions()) {
-                    realisonLaQuete(queteEnCour, i, solution, scenarioListe);
-                    System.out.println(queteEnCour+"\nOk");
-                    Arrays.toString(quetesRealisee);
-                    System.out.println(experienceAccumulee);
-                }
-                if (queteEnCour.possedePreconditions()) {
-                    int nbPreconditions = queteEnCour.nbPreconditions();
-                    int [][] preconditions = queteEnCour.getChPreconditions();
-                    if (nbPreconditions == 1) {
-                        realisationLaQueteSiUnePrecondition(preconditions, queteEnCour, i, solution, scenarioListe);
-                        System.out.println(queteEnCour+"\nOk");
-                        Arrays.toString(quetesRealisee);
-                        System.out.println(experienceAccumulee);
-                    }
-                    if (nbPreconditions == 2) {
-                        realisationLaQueteSiDeuxPreconditions(preconditions, queteEnCour, i, solution, scenarioListe);
-                        System.out.println(queteEnCour+"\nOk");
-                        Arrays.toString(quetesRealisee);
-                        System.out.println(experienceAccumulee);
+                // traitement de la quete finale
+                if (queteEnCour == queteFinale) {
+                    if (experienceAccumulee >= experienceNecessaireQueteFinale) {
+                        realisonLaQuete(queteEnCour, solution);
                     }
                 }
             }
-        }
-        if (!Arrays.asList(quetesRealisee).contains(queteFinale)) {
-            solution[solution.length-1] = queteFinale;
-            quetesRealisee[quetesRealisee.length-1] = queteFinale.getChNumero();
-            scenarioListe.remove(queteFinale);
         }
 
         return solution;
