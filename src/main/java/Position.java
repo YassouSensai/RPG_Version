3de833ec101068f5602414db57f643d16b7a1168
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-05-06 21:13:44 +0200

    mise a jour de la classe

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -4,23 +4,15 @@ public class Position {
     int x;
     int y;
 
+
     /**
-     * Constructeur de la classe Position avec les coordonees en parametre
+     * Constructeur de la classe Position à partir d'une quete
      * @param parX
      * @param parY
      */
     public Position(int parX, int parY) {
         x = parX;
         y = parY;
-    }
-
-    /**
-     * Constructeur de la classe Position à partir d'une quete
-     * @param parQuete
-     */
-    public Position(Quete parQuete) {
-        x = parQuete.getChX();
-        y = parQuete.getChY();
     }
 
     /**
